08490235b45fa345edc12931cef560268495e9d9
temporalzone <temporalzone9@gmail.com>
2026-03-14 23:53:38 +0530

    render settings fix

--- oshochatbot/settings.py
+++ oshochatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cf0%m(idom^q!k+sgrv=&ucsx@xh6g0y8_j3)%4vt7xo5m51gv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
